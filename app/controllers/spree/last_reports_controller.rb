820c8dd9f5ae8a60fc4754fab8f8b1f6b0d502c7
LWS <alexandredt79@gmail.com>
2021-02-12 15:33:54 -0500

    fixer le fetcher de reports

--- app/controllers/spree/last_reports_controller.rb
+++ app/controllers/spree/last_reports_controller.rb
@@ -3,7 +3,9 @@ module Spree
 
 
     def show
-      data = open(Spree::LastReport::last.url)
+
+      data = open(Spree::LastReport::last.url.sub ENV['S3_BUCKET_NAME']+".s3.amazonaws.com", ENV["CLOUDFRONT_ENDPOINT"])
+
       send_data data.read, filename: "products.xml", type: "application/xml", disposition: 'attachment', stream: 'true', buffer_size: '4096'
 
     end
